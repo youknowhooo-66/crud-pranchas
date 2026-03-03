2b289beb19eefbb55c91f10a19086c3f37f258a4
MAYCON GIBSON NEVES DOS SANTOS <maycon_g_santos@estudante.sesisenai.org.br>
2026-03-02 21:52:16 -0300

    ...

--- crud-pranchas/src/routes/pranchaRoutes.js
+++ crud-pranchas/src/routes/pranchaRoutes.js
@@ -4,15 +4,17 @@ import {
   listarPranchas,
   buscarPrancha,
   atualizarPrancha,
-  deletarPrancha
+  deletarPrancha,
+  deletarVariasPranchas
 } from '../controllers/pranchaController.js';
 
 const router = express.Router();
 
-router.post('/', criarPrancha);
-router.get('/', listarPranchas);
-router.get('/:id', buscarPrancha);
-router.put('/:id', atualizarPrancha);
-router.delete('/:id', deletarPrancha);
+router.post('/', criarPrancha);           // Criar
+router.get('/', listarPranchas);          // Listar todas
+router.get('/:id', buscarPrancha);        // Buscar por ID
+router.patch('/:id', atualizarPrancha);   // Atualizar atributos
+router.delete('/:id', deletarPrancha);    // Deletar 1 prancha
+router.delete('/', deletarVariasPranchas);// Deletar múltiplas
 
 export default router;
